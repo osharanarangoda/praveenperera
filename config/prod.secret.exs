48d42940219e12a7fe37a5009e34d6aa2b09e1bf
Praveen <ppraveen25@gmail.com>
2015-09-06 19:26:02 -0400

    changed prod.secret.ex
    changed prod.secret.exs

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -7,6 +7,6 @@ config :praveenperera, Praveenperera.Endpoint,
   secret_key_base: "LMsWVbuVHbv+ptuAEI6cMmPcChJ6w/xlN7AS9WwD4dzA0AaP+tpbmFgp5wdmkFF5"
 
 # Configure your database
-config :elixir_app_name, TbTutorial.Repo,
+config :praveenperera, TbTutorial.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: {:system, "DATABASE_URL"}
