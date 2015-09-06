7e8cbfcec3ce53cd13dff2671d1bc3ff62345fc7
Praveen <ppraveen25@gmail.com>
2015-09-06 19:16:34 -0400

    prod.secrect.exs added
    prod.secrect.exs added

---
+++ config/prod.secret.exs
@@ -0,0 +1,12 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :praveenperera, Praveenperera.Endpoint,
+  secret_key_base: "LMsWVbuVHbv+ptuAEI6cMmPcChJ6w/xlN7AS9WwD4dzA0AaP+tpbmFgp5wdmkFF5"
+
+# Configure your database
+config :elixir_app_name, TbTutorial.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: {:system, "DATABASE_URL"}
