6d61df439c6bf0764492d24371d21aea9f13494a
Praveen <ppraveen25@gmail.com>
2015-09-06 20:02:14 -0400

    dokku
    dokku

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,9 +4,9 @@ use Mix.Config
 # you likely want to automate and keep it away from
 # your version control system.
 config :praveenperera, Praveenperera.Endpoint,
-  secret_key_base: "LMsWVbuVHbv+ptuAEI6cMmPcChJ6w/xlN7AS9WwD4dzA0AaP+tpbmFgp5wdmkFF5"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
 config :praveenperera, Praveenperera.Repo,
 	adapter: Ecto.Adapters.Postgres,
-	url: {:system, "DATABASE_URL"}
+	url: System.get_env("DATABASE_URL")
