0bca02774a3963fdc3f45d762ef13458417025f1
Praveen <ppraveen25@gmail.com>
2015-09-08 14:27:02 -0400

    postcss and autoprefixer added to brunch and configured

--- brunch-config.js
+++ brunch-config.js
@@ -59,6 +59,11 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/web\/static\/vendor/]
+    },
+    postcss: {
+        processors: [
+            require('autoprefixer')(['last 8 versions']),
+        ]
     }
   },
 
