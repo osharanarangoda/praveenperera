7143dee89c3c122ccfbc10249342605df3396fe9
Praveen <ppraveen25@gmail.com>
2015-09-21 17:15:03 -0400

    ga

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -35,8 +35,7 @@ import  "web/static/js/contact"
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-67904702-1', 'auto');  // Replace with your property ID.
+ga('create', 'UA-67904702-1', 'auto');
 ga('send', 'pageview');
-
 //<!-- End Google Analytics -->
 
