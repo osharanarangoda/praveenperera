ba8a9a4ab17900115123df242f727f580a103027
Praveen <ppraveen25@gmail.com>
2016-04-29 05:21:23 -0400

    added config for mine mailgun and oshie mailgun

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -12,6 +12,11 @@ config :praveenperera, Praveenperera.Repo,
 	url: System.get_env("DATABASE_URL"),
 	pool_size: 20
 
-# Configure mailgun
+# Configure mailgun praveen
 config :praveenperera, mailgun_domain: System.get_env("MAILGUN_DOMAIN"),
                        mailgun_key: System.get_env("MAILGUN_KEY")
+
+# Configure mailgun oshie
+config :praveenperera, o_mailgun_domain: System.get_env("O_MAILGUN_DOMAIN"),
+                      o_mailgun_key: System.get_env("O_MAILGUN_KEY"),
+                      o_email: System.get_env("O_EMAIL")
