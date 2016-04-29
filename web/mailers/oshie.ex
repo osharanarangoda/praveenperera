8680e3bd413490c1084252274e9198a9e54212a5
Praveen <ppraveen25@gmail.com>
2016-04-29 05:22:27 -0400

    separated two mailers into own files

---
+++ web/mailers/oshie.ex
@@ -0,0 +1,19 @@
+defmodule Praveenperera.OshieMailer do
+
+  use Mailgun.Client, domain: Application.get_env(:praveenperera, :o_mailgun_domain),
+                      key:    Application.get_env(:praveenperera, :o_mailgun_key)
+
+  @to Application.get_env(:praveenperera, :o_email)
+
+  def send_contact_email(name, email, number) do
+    send_email to: @to,
+               from: email,
+               subject: "New Lanka Novels Customer Submission",
+               html: """
+                <p><b>Name:</b>#{name}</p>
+                <p><b>Email:</b>#{email}</p>
+                <p><b>Phone Number:</b>#{number}</p>
+               """
+  end
+
+end
